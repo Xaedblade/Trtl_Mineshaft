6f9ac15a7c145df197ceb1fda3c1f99caa1a030a
Chad Vandermark <chad@vega.works>
2017-05-15 16:35:26 +1200

    tested and added the up and down functions and working as desired time to merge to basic

--- Trtl_Mineshaft.lua
+++ Trtl_Mineshaft.lua
@@ -1,17 +1,20 @@
-x=0
-z=0
-y=0
+x=0; z=0; y=0
 direction = 0
 
 -----------------------------------------------------
---This section is for general movement functions
+-- This section is for general movement functions
+-- These functions keeps track of the direction
+-- and location of the turtle
+-----------------------------------------------------
 
---*********** changeDirection
--- This function keeps track of the direction of the turtle when the turtle turns
-function changeDirection(change)
+-- updates the direction of the turtle regardless
+-- of how many turns by mod 4
+function updateDirection(change)
 	direction = (direction + change)% 4
 end
 
+-- updates the coordinates of the turtle after
+-- moving based on the direction
 function updateCoords(_direction)
 	if direction == 0 then
 		x=x+1
@@ -24,20 +27,20 @@ function updateCoords(_direction)
 	end
 end
 
-function updateHeight(change,_direction)
-	print("Going ".._direction.." "..change)
-	if _direction=="up" then
-		y=y+change
-	else
-		y=y-change
-	end
+-- updates the turtle's y coordinate by number
+-- provided
+function updateHeight(change)
+	y=y+change
 end
 
---*********** forward
+-----------------------------------------------------
+-- 
+-- 
+-----------------------------------------------------
 -- This function takes one paramater and will 
 -- move the turtle forward x blocks and clear 
--- blocks in front of it and attack entities in
--- front of it
+-- blocks in front of it and attack entities 
+-- in front of it
 function forward(times)
 	for i=1,times do
 		
@@ -57,33 +60,77 @@ function forward(times)
 	end
 end
 
-function turn(direction, times)
+function turn(direction,times)
 	for i=1,times do
 		if direction=="left" then
 			--turtle.turnLeft()
-			changeDirection(-1)
+			updateDirection(-1)
 		else
 			--turtle.turnRight()
-			changeDirection(1)
+			updateDirection(1)
 		end
 	end
 end
 
 function down(times)
+	times = times * -1
+	updateHeight(times)
 end
 
 function up(times)
+	updateHeight(times)
 end
 
 function makeShaft()
+
 end
 
+-----------------------------------------------------
+-- Tests
+-----------------------------------------------------
 
 
--- test 3
-print("Test 3")
-print("Starting fr: d="..direction.." => 4 | x="..x.." | z="..z.." | y="..y)
-updateHeight(2,"up")
-updateHeight(2,"down")
-updateHeight(5,"down")
-print("updating to: d="..direction.." => 4 | x="..x.." | z="..z.." | y="..y)
+function test3()
+
+	print("Test 3")
+	print("Starting fr: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	up(2)
+	print("update height: 2")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	down(2)
+	print("update height: -2")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	down(5)
+	print("update height: -5")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+
+	print(" ")
+	print("Expected Results")
+	y=0
+	print("Starting fr: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	updateHeight(2)
+	print("update height: 2")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	updateHeight(-2)
+	print("update height: -2")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+	updateHeight(-5)
+	print("update height: -5")
+	print("updating to: d="..direction.." | x="..
+	x.." | z="..z.." | y="..y)
+end
+
+function test2()
+end
+
+function test1()
+end
+
+test3()
