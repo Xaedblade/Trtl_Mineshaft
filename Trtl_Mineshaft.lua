309f1fb4049afed45586fbe002b5027cca7d8096
Chad Vandermark <chad@vega.works>
2017-05-15 13:02:56 +1200

    added updateDirection

--- Trtl_Mineshaft.lua
+++ Trtl_Mineshaft.lua
@@ -24,16 +24,26 @@ function updateCoords(_direction)
 	end
 end
 
+function updateHeight(change)
+	y=y+change
+end
+
 --*********** forward
--- This function takes one paramater and will move the 
--- turtle forward x blocks and clear blocks in front of
--- it and attack entities in front of it
+-- This function takes one paramater and will 
+-- move the turtle forward x blocks and clear 
+-- blocks in front of it and attack entities in
+-- front of it
 function forward(times)
-	print("Moving forward "..times.." times")
-    for i=1,times do
-		while not true do--turtle.forward() do -- can't move forward so make a check for a block
+	for i=1,times do
+		
+		--turtle.forward() do -- can't move
+		--forward so make a check for a block
+		while not true do
 			print("Blocked")
-			if turtle.detect() then -- there is not a block must be an entity there for attack
+
+			-- there is not a block must
+			-- be an entity there for attack
+			if turtle.detect() then 
 				print("Attacking entity")
 				turtle.attack()
 			end
@@ -43,7 +53,6 @@ function forward(times)
 end
 
 function turn(direction, times)
-	print("Turning "..direction.." "..times.." times")
 	for i=1,times do
 		if direction=="left" then
 			--turtle.turnLeft()
@@ -64,18 +73,24 @@ end
 function makeShaft()
 end
 
+
+
 -- test 2
 print("Test 2")
+print("Starting fr: d="..direction.." => 4 | x="..x.." | z="..z)
 turn('right',4)
-print("Starting coords from: x="..x.." | z="..z)
+print("d = "..direction)
 forward(5)
-print("Updating coords to: x="..x.." | z="..z)
+print("Updating to: d="..direction.." => 5 | x="..x.." | z="..z)
 turn('left',3)
+print("d = "..direction)
 forward(1)
-print("Updating coords to: x="..x.." | z="..z)
+print("Updating to: d="..direction.." => 1 | x="..x.." | z="..z)
 turn('right',1)
+print("d = "..direction)
 forward(1)
-print("Updating coords to: x="..x.." | z="..z)
+print("Updating to: d="..direction.." => 1 | x="..x.." | z="..z)
 turn('right',1)
+print("d = "..direction)
 forward(6)
-print("Updating coords to: x="..x.." | z="..z)
+print("Updating to: d="..direction.." => 6 | x="..x.." | z="..z)
